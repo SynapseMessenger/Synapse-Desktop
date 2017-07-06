57479feff2f7b5b8cf8235e370820dab39ea6496
MarFerPra <mfernandezpranno@gmail.com>
2017-07-06 22:52:02 +0200

    Refactored contacts, connection and event listening funcionality, work in progress on chat component. #29

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -14,35 +14,18 @@ import { bindActionCreators } from 'redux';
 import Conversation from './Conversation';
 import MessageInput from './MessageInput';
 
-import { updateNavbar } from '../actions/navbarActions';
 import { sendInitChat } from '../actions/chatActions';
 import {
   addMessageToChat, receivedAcceptChat, sendAcceptChat
 } from '../actions/conversationsActions';
 
 class Chat extends React.Component {
-  constructor(props) {
-    super(props);
-    this.updateChat = this.updateChat.bind(this);
-  }
-
-  componentDidMount() {
-    const {
-      updateNavbar, receiver,
-      chatClient, conversation,
-      user
-    } = this.props;
-
-    updateNavbar(receiver.username, '/synapse/contacts');
-    chatClient.updateView = this.updateChat;
-
-    if (!conversation || conversation.length === 0) {
-      chatClient.initChat(user._id);
-    }
-  }
 
   render() {
-    const { user, receiver } = this.props;
+    const { user, receiver, conversation } = this.props;
+    if (!conversation || conversation.length === 0) {
+      this.props.sendInitChat(user._id);
+    }
     return (
       <div className="container">
         <Conversation receiverId={receiver._id} />
@@ -54,22 +37,16 @@ class Chat extends React.Component {
 
 const mapStateToProps = (state, ownProps) => {
   const receiverId = ownProps.match.params.userId;
-  const { onlineUsers, offlineUsers } = state.chat;
-  const allUsers = onlineUsers.concat(offlineUsers);
+  const { onlineUsers, offlineUsers, allUsers } = state.chat;
   return {
     receiver: allUsers.find((user) => user._id === receiverId),
     user: state.user,
-    chatClient: state.chat.client,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
-    updateNavbar,
-    sendInitChat,
-    sendAcceptChat,
-    addMessageToChat,
-    receivedAcceptChat
+    sendInitChat
   }, dispatch);
 };
 
